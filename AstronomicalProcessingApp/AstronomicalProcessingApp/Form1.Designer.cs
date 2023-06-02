d4208e4a3b3396de5f195aa131df8b0e722784eb
James <58790261+JamesChellew@users.noreply.github.com>
2023-06-02 10:48:02 +0800

    Update my branch (#25)
    * Add ignore file
    
    * remove obj files
    
    * Binary search algorithm
    
    * Correction of Binary Search Algorithm
    
    * Added Method area for randomise function (test commit and push)
    
    * RndFill Method completed
    
    * (Test) Added Edit button on click
    
    * Added listBoxDisplay Click Method
    
    * (Test) Corrected selected items methods
    
    * Delete .gitignore
    
    * Include Previously Ignored files
    
    * Test
    
    * Test
    
    * re-add git ignore
    
    * Delete AstronomicalProcessingApp/AstronomicalProcessingApp/.vs/AstronomicalProcessingApp directory
    
    * Delete AstronomicalProcessingApp/AstronomicalProcessingApp/bin/Debug/net6.0-windows directory
    
    * Delete AstronomicalProcessingApp/AstronomicalProcessingApp/obj directory
    
    * Delete .vs directory
    
    * another test
    
    * remove second test comment
    
    * Edit Button Functional, UpdateList Method
    
    * Updating Search method + commenting
    
    * Test
    
    * Update README.md
    
    * Psudo Code and Algorithms added
    
    * Stage 1 of gui design
    
    * Updated GUI finish
    
    * Sprint One Done
    
    * GUI Design Spec Finished
    
    * Mode Button Method Finished
    
    * Linear Search Finished
    
    * Bubble Sort Added
    
    * Extended listBox to display more data
    
    * test
    
    * Change to .gitignore to ignore .docx files
    
    * Corrected
    
    * Delete Assessment Task Three 2023.v1.0 - Handover(1).docx
    
    * Delete Assessment Task Three 2023.v1.0 - SprintOne.docx
    
    * Delete Assessment Task Three 2023.v1.0 - SprintTwo.docx
    
    * Fix Linear Search Button
    
    * linear search refrence
    
    * Hopefully fix referencing of button methods
    
    * fix
    
    * updated gui?
    
    * User Feedback Box
    
    * Changed Binary search to fix loop error
    
    * .
    
    * Fix Bin Search Not found message
    
    * Edit User Feedback Message
    
    * ToolTips added
    
    * Add comments to each block of code.
    
    * Adjust Gui to fit all 24 numbers
    
    * Modified Mid-Extreme to produce a double + fix edge case
    
    ---------
    
    Co-authored-by: Josh Barker <joshbarker5@outlook.com>

--- AstronomicalProcessingApp/AstronomicalProcessingApp/Form1.Designer.cs
+++ AstronomicalProcessingApp/AstronomicalProcessingApp/Form1.Designer.cs
@@ -28,7 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            buttonSearch = new Button();
+            components = new System.ComponentModel.Container();
+            buttonBinarySearch = new Button();
             buttonSort = new Button();
             buttonEdit = new Button();
             textBoxInput = new TextBox();
@@ -37,63 +38,81 @@
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
+            textBoxResult = new TextBox();
+            label4 = new Label();
+            buttonLinearSearch = new Button();
+            buttonAverage = new Button();
+            buttonMode = new Button();
+            buttonMidExtreme = new Button();
+            buttonRange = new Button();
+            toolTip1 = new ToolTip(components);
             SuspendLayout();
             // 
-            // buttonSearch
+            // buttonBinarySearch
             // 
-            buttonSearch.Location = new Point(214, 84);
-            buttonSearch.Name = "buttonSearch";
-            buttonSearch.Size = new Size(94, 29);
-            buttonSearch.TabIndex = 0;
-            buttonSearch.Text = "Search";
-            buttonSearch.UseVisualStyleBackColor = true;
+            buttonBinarySearch.Location = new Point(195, 12);
+            buttonBinarySearch.Name = "buttonBinarySearch";
+            buttonBinarySearch.Size = new Size(107, 29);
+            buttonBinarySearch.TabIndex = 0;
+            buttonBinarySearch.Text = "Binary Search";
+            buttonBinarySearch.UseVisualStyleBackColor = true;
+            buttonBinarySearch.Click += buttonSearch_Click;
+            buttonBinarySearch.MouseHover += buttonBinarySearch_MouseHover;
             // 
             // buttonSort
             // 
-            buttonSort.Location = new Point(214, 124);
+            buttonSort.Location = new Point(195, 87);
             buttonSort.Name = "buttonSort";
-            buttonSort.Size = new Size(94, 29);
+            buttonSort.Size = new Size(107, 29);
             buttonSort.TabIndex = 1;
             buttonSort.Text = "Sort";
             buttonSort.UseVisualStyleBackColor = true;
+            buttonSort.Click += buttonSort_Click;
+            buttonSort.MouseHover += buttonSort_MouseHover;
             // 
             // buttonEdit
             // 
-            buttonEdit.Location = new Point(214, 159);
+            buttonEdit.Location = new Point(307, 31);
             buttonEdit.Name = "buttonEdit";
             buttonEdit.Size = new Size(94, 29);
             buttonEdit.TabIndex = 2;
             buttonEdit.Text = "Edit";
             buttonEdit.UseVisualStyleBackColor = true;
+            buttonEdit.Click += buttonEdit_Click;
+            buttonEdit.MouseHover += buttonEdit_MouseHover;
             // 
             // textBoxInput
             // 
-            textBoxInput.Location = new Point(12, 31);
+            textBoxInput.Location = new Point(13, 31);
             textBoxInput.Name = "textBoxInput";
             textBoxInput.Size = new Size(177, 27);
             textBoxInput.TabIndex = 3;
+            textBoxInput.MouseHover += textBoxInput_MouseHover;
             // 
             // listBoxDisplay
             // 
             listBoxDisplay.FormattingEnabled = true;
             listBoxDisplay.ItemHeight = 20;
-            listBoxDisplay.Location = new Point(12, 84);
+            listBoxDisplay.Location = new Point(13, 84);
             listBoxDisplay.Name = "listBoxDisplay";
-            listBoxDisplay.Size = new Size(177, 104);
+            listBoxDisplay.Size = new Size(177, 484);
             listBoxDisplay.TabIndex = 4;
+            listBoxDisplay.SelectedIndexChanged += listBoxDisplay_SelectedIndexChanged;
+            listBoxDisplay.MouseHover += listBoxDisplay_MouseHover;
             // 
             // textBoxMessage
             // 
-            textBoxMessage.Location = new Point(12, 215);
+            textBoxMessage.Location = new Point(195, 541);
             textBoxMessage.Name = "textBoxMessage";
             textBoxMessage.ReadOnly = true;
             textBoxMessage.Size = new Size(177, 27);
             textBoxMessage.TabIndex = 5;
+            textBoxMessage.MouseHover += textBoxMessage_MouseHover;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 8);
+            label1.Location = new Point(13, 8);
             label1.Name = "label1";
             label1.Size = new Size(72, 20);
             label1.TabIndex = 6;
@@ -102,7 +121,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 61);
+            label2.Location = new Point(13, 61);
             label2.Name = "label2";
             label2.Size = new Size(148, 20);
             label2.TabIndex = 7;
@@ -111,17 +130,95 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(12, 191);
+            label3.Location = new Point(193, 518);
             label3.Name = "label3";
             label3.Size = new Size(96, 20);
             label3.TabIndex = 8;
             label3.Text = "User Feeback";
             // 
+            // textBoxResult
+            // 
+            textBoxResult.Location = new Point(195, 189);
+            textBoxResult.Name = "textBoxResult";
+            textBoxResult.ReadOnly = true;
+            textBoxResult.Size = new Size(207, 27);
+            textBoxResult.TabIndex = 9;
+            textBoxResult.MouseHover += textBoxResult_MouseHover;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(195, 167);
+            label4.Name = "label4";
+            label4.Size = new Size(159, 20);
+            label4.TabIndex = 10;
+            label4.Text = "Data Processing Result";
+            // 
+            // buttonLinearSearch
+            // 
+            buttonLinearSearch.Location = new Point(195, 47);
+            buttonLinearSearch.Name = "buttonLinearSearch";
+            buttonLinearSearch.Size = new Size(107, 29);
+            buttonLinearSearch.TabIndex = 11;
+            buttonLinearSearch.Text = "Linear Search";
+            buttonLinearSearch.UseVisualStyleBackColor = true;
+            buttonLinearSearch.Click += buttonLinearSearch_Click;
+            buttonLinearSearch.MouseHover += buttonLinearSearch_MouseHover;
+            // 
+            // buttonAverage
+            // 
+            buttonAverage.Location = new Point(195, 221);
+            buttonAverage.Name = "buttonAverage";
+            buttonAverage.Size = new Size(94, 29);
+            buttonAverage.TabIndex = 12;
+            buttonAverage.Text = "Average";
+            buttonAverage.UseVisualStyleBackColor = true;
+            buttonAverage.MouseHover += buttonAverage_MouseHover;
+            // 
+            // buttonMode
+            // 
+            buttonMode.Location = new Point(295, 221);
+            buttonMode.Name = "buttonMode";
+            buttonMode.Size = new Size(103, 29);
+            buttonMode.TabIndex = 13;
+            buttonMode.Text = "Mode";
+            buttonMode.UseVisualStyleBackColor = true;
+            buttonMode.Click += buttonMode_Click;
+            buttonMode.MouseHover += buttonMode_MouseHover;
+            // 
+            // buttonMidExtreme
+            // 
+            buttonMidExtreme.Location = new Point(295, 257);
+            buttonMidExtreme.Name = "buttonMidExtreme";
+            buttonMidExtreme.Size = new Size(103, 29);
+            buttonMidExtreme.TabIndex = 14;
+            buttonMidExtreme.Text = "Mid-Extreme";
+            buttonMidExtreme.UseVisualStyleBackColor = true;
+            buttonMidExtreme.Click += buttonMidExtreme_Click;
+            buttonMidExtreme.MouseHover += buttonMidExtreme_MouseHover;
+            // 
+            // buttonRange
+            // 
+            buttonRange.Location = new Point(195, 257);
+            buttonRange.Name = "buttonRange";
+            buttonRange.Size = new Size(94, 29);
+            buttonRange.TabIndex = 15;
+            buttonRange.Text = "Range";
+            buttonRange.UseVisualStyleBackColor = true;
+            buttonRange.MouseHover += buttonRange_MouseHover;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(332, 261);
+            ClientSize = new Size(410, 583);
+            Controls.Add(buttonRange);
+            Controls.Add(buttonMidExtreme);
+            Controls.Add(buttonMode);
+            Controls.Add(buttonAverage);
+            Controls.Add(buttonLinearSearch);
+            Controls.Add(label4);
+            Controls.Add(textBoxResult);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -130,7 +227,7 @@
             Controls.Add(textBoxInput);
             Controls.Add(buttonEdit);
             Controls.Add(buttonSort);
-            Controls.Add(buttonSearch);
+            Controls.Add(buttonBinarySearch);
             Name = "Form1";
             Text = "Astronomical Processing";
             ResumeLayout(false);
@@ -139,7 +236,7 @@
 
         #endregion
 
-        private Button buttonSearch;
+        private Button buttonBinarySearch;
         private Button buttonSort;
         private Button buttonEdit;
         private TextBox textBoxInput;
@@ -148,5 +245,13 @@
         private Label label1;
         private Label label2;
         private Label label3;
+        private TextBox textBoxResult;
+        private Label label4;
+        private Button buttonLinearSearch;
+        private Button buttonAverage;
+        private Button buttonMode;
+        private Button buttonMidExtreme;
+        private Button buttonRange;
+        private ToolTip toolTip1;
     }
 }
--- AstronomicalProcessingApp/AstronomicalProcessingApp/Form1.cs
+++ AstronomicalProcessingApp/AstronomicalProcessingApp/Form1.cs
@@ -1,10 +1,322 @@
+using System;
+
 namespace AstronomicalProcessingApp
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
+            // Winform initialise
             InitializeComponent();
+            // Neutrino Data Initialise
+            RdmFill();
+            UpdateDisplay();
+        }
+        // Global Variables
+        static int arraySize = 24;
+        int[] neutInteractions = new int[arraySize];
+
+        // Method fills array with 24 random numbers using random object
+
+        void RdmFill()
+        {
+            Random random = new Random();
+            for (int i = 0; i < arraySize; i++)
+            {
+                neutInteractions[i] = random.Next(10, 99);
+            }
+
+        }
+
+        // Clears listBoxDisplay and then writes neutInteractions array to listBoxDisplay
+
+        void UpdateDisplay()
+        {
+            listBoxDisplay.Items.Clear();
+            for (int i = 0; i < arraySize; i++)
+            {
+
+                listBoxDisplay.Items.Add(neutInteractions[i]);
+            }
+        }
+        // Binary Search Method on button click
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            int mid;
+            int max = arraySize - 1;
+            int min = 0;
+            if (Int32.TryParse(textBoxInput.Text, out int key))
+            {
+                while (min <= max)
+                {
+                    mid = (min + max) / 2;
+                    if (neutInteractions[mid] == key)
+                    {
+                        textBoxMessage.Text = (key + " Found!");
+                        break;
+                    }
+                    else if (neutInteractions[mid] < key)
+                    {
+                        min = mid + 1;
+                    }
+                    else if (neutInteractions[mid] > key)
+                    {
+                        max = mid - 1;
+                    }
+                    textBoxMessage.Text = (key + " Not Found!");
+                }
+            }
+            else
+            {
+                textBoxMessage.Text = ("Invalid Input");
+            }
+        }
+        // Edit Button
+        // Parses user input from textBoxInput to integer. Check if is integer and checks if it is not white space.
+        // Using listBox index value, replaces corresponding value in the array with parsed input from user then updates the array.
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            string text = textBoxInput.Text;
+
+            if (listBoxDisplay.SelectedIndex != -1)
+            {
+                if (Int32.TryParse(text, out int input) && !(String.IsNullOrWhiteSpace(text)))
+                {
+                    int k = listBoxDisplay.SelectedIndex;
+
+                    neutInteractions[k] = input;
+                    UpdateDisplay();
+                    textBoxInput.Clear();
+                    textBoxInput.Clear();
+                }
+                else
+                {
+                    textBoxMessage.Text = ("Invalid Change!");
+                    listBoxDisplay.ClearSelected();
+                    textBoxInput.Clear();
+                }
+            }
+        }
+        // Upon selecting value from list box, display it in the input box
+        private void listBoxDisplay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxDisplay.SelectedIndex != -1)
+            {
+                textBoxInput.Text = neutInteractions[listBoxDisplay.SelectedIndex].ToString();
+            }
+            else
+            {
+                listBoxDisplay.ClearSelected();
+                textBoxInput.Clear();
+            }
+        }
+
+        //
+        // Mode Button
+        // Double Foreach loop:
+        // The first one gives the target value
+        // The second gives the comparing value
+        // For each Target value, we will iterate through the array for comparing values
+        // Each time the comparing number matches the target, increase the count by 1
+        // After comparing each of the numbers once, if the count exceeds the maxcount, we will clear a list add that value to the list
+        // if that value had the same count as the max count, we will just add the value to the list instead.
+        // Lastly, we will use a for loop to display the results in a readable format.
+        //
+        private void buttonMode_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Clear();
+            int maxCount = 0;
+            List<int> modes = new List<int>();
+            foreach (int i in neutInteractions)
+            {
+                int count = 0;
+                foreach (int j in neutInteractions)
+                {
+                    if ((i == j))
+                    {
+                        count++;
+                    }
+                }
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    modes.Clear();
+                    modes.Add(i);
+                }
+                else if (count == maxCount && !modes.Contains(i))
+                {
+                    modes.Add(i);
+                }
+            }
+            int length = modes.Count;
+            for (int i = 0; i < length; i++)
+            {
+                textBoxResult.AppendText(modes[i].ToString());
+                if (i != length - 1)
+                {
+                    textBoxResult.AppendText(", ");
+                }
+            }
+
+        }
+        // Mid-Extreme Buton
+        // Iterates through each value of array,
+        // Sets a new min or max if the number compared to it is lower/higher for that loop,
+        // Calculates average of max and min (the mid extreme),
+        // Displays result in textBoxResult
+        private void buttonMidExtreme_Click(object sender, EventArgs e)
+        {
+            textBoxResult.Clear();
+            int max = neutInteractions[0];
+            int min = neutInteractions[0];
+            double mid;
+            foreach (int i in neutInteractions)
+            {
+                if (i < min)
+                {
+                    min = i;
+                }
+                else if (i > max)
+                {
+                    max = i;
+                }
+            }
+            mid = (max + min) / 2.0; // Must have a double in calculation to get double result.
+            textBoxResult.Text = mid.ToString();
+        }
+        // Linear search
+        // Checks if the key is a valid integer,
+        // loops through each value of the array,
+        // compares each value to the key,
+        // returns message if key matches result, and if it doesnt.
+        private void buttonLinearSearch_Click(object sender, EventArgs e)
+        {
+            bool itemFound = false;
+            if (!String.IsNullOrWhiteSpace(textBoxInput.Text) && Int32.TryParse(textBoxInput.Text, out int key))
+            {
+                foreach (int i in neutInteractions)
+                {
+                    if (key == i)
+                    {
+                        itemFound = true;
+                        break;
+                    }
+                }
+            }
+            if (itemFound)
+            {
+                textBoxMessage.Text = (textBoxInput.Text + " Found in Array!");
+            }
+            else
+            {
+                textBoxMessage.Text = (textBoxInput.Text + " Not Found in Array!");
+            }
+
+        }
+        // Sort Button
+        // Sorts neutrino interactions in assending order on button press.
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            int max = arraySize;
+            int temp;
+
+            for (int pass = 0; pass < max - 1; pass++)
+            {
+                for (int index = 0; index < (max - pass - 1); index++)
+                {
+                    if (neutInteractions[index] > neutInteractions[index + 1])
+                    {
+                        temp = neutInteractions[index];
+                        neutInteractions[index] = neutInteractions[index + 1];
+                        neutInteractions[index + 1] = temp;
+                    }
+                }
+            }
+            UpdateDisplay();
+        }
+        //
+        // Below here is all the tool tips for each GUI element. Move your code above it for the sake of readability.
+        // The @ symbol just makes the string a literal, it reads each character as it is (i.e. each new line is a new line for the tooltip)
+        //
+        private void textBoxInput_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show(
+                @"Input a number you would like to search for here.
+or
+Select an entry and edit it here to chage the number.", textBoxInput);
+        }
+
+        private void buttonBinarySearch_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show(
+                @"Binary searches the array of numbers using the number in the input box as the key.
+The search will tell you if number exists in array.", buttonBinarySearch);
+        }
+
+        private void buttonLinearSearch_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show(
+                @"Sequentially searches the array of numbers using the number in the input box as the key.
+The search will tell you if number exists in array.", buttonLinearSearch);
+        }
+
+        private void buttonEdit_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show(
+                @"Select a piece of data you would like to change from the list of neutrino interactions,
+change the data in to the desired value in the input text box,
+press the edit button to submit the changes to the array.", buttonEdit);
+        }
+
+        private void buttonSort_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show("Sorts the data in assending order upon button press.", buttonSort);
+        }
+
+        private void listBoxDisplay_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show(
+                @"Displays neutrino information data.
+Click on a value to select it.", listBoxDisplay);
+        }
+
+        private void textBoxResult_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show("Result of mathematical opperations will appear here", textBoxResult);
+        }
+
+        private void buttonAverage_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show(
+                @"Calculates average neutrino interaction pre hour
+Equation:
+Sum of interactions/24", buttonAverage);
+        }
+
+        private void buttonMode_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show(@"Calculates the mode number of neutrino interactions from the day", buttonMode);
+        }
+
+        private void buttonRange_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show(
+                @"Calculates the range of neutrino interactions
+Equation:
+Max interactions - Min interactions", buttonRange);
+        }
+
+        private void buttonMidExtreme_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show(
+                @"Calculates the midpoint of the two most extreme numbers
+Equation:
+(Max interactions + Min interactions)/2", buttonMidExtreme);
+        }
+
+        private void textBoxMessage_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show("Each action/error will display a message here", textBoxMessage);
         }
     }
 }
